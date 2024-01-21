081d42189db18aebb7863c936cf68cf821c7f994
BriAnna Slik <daffadelicious@gmail.com>
2024-01-20 22:05:12 -0700

    Finished assignment

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -7,5 +7,6 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-detail.component.css'
 })
 export class ContactDetailComponent {
-  public contact = new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-296-3771", "../../../assets/images/jacksonk.jpg", null)
+  // public contact = new Contact(1, "R. Kent Jackson", "jacksonk@byui.edu", "208-296-3771", "../../../assets/images/jacksonk.jpg", null)
+  public contact = new Contact(1, "", "", "", "", null)
 }
