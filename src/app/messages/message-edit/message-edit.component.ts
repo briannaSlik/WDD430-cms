9004b625a61bca6800e214484c67f7affd20df81
BriAnna Slik <daffadelicious@gmail.com>
2024-01-27 21:43:54 -0700

    finished message functionality

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  // subjectText = 'Test';
+  // messageText = 'TestText';
+  @ViewChild('subject', {static: true}) subject: ElementRef;
+  @ViewChild('msgText', {static: true}) msgText: ElementRef;
 
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  currentSender: string = "BriAnna Slik"
+
+  onSendMessage(){
+    let subject: string = this.subject.nativeElement.value;
+    let message: string = this.msgText.nativeElement.value;
+    let newMessage: Message = new Message(1, subject, message, this.currentSender)
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear(){
+    this.subject.nativeElement.value = "";
+    this.msgText.nativeElement.value = "";
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  public messages: Message[] = [
+    new Message(1, "Test Message", "Wow it worked!", "BriAnna Slik"),
+    new Message(2, "Another Test", "Woo hoo! A second one!", "BriAnna Slik"),
+    new Message(3, "You'd never guess", "...but it's another test!", "BriAnna Slik")
+  ];
 
+  onAddMessage(message: Message){
+    this.messages.push(message);
+  }
 }
